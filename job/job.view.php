319e74d325ae603dbeda9fe3908137e60ff0cb72
MigueValiente <miguevaliente96@gmail.com>
2018-11-28 14:40:50 +0100

    añadido jobs

---
+++ job/job.view.php
@@ -0,0 +1,7 @@
+<?php
+    require_once '../setup.php';
+    require_once '../includes/header.php'; 
+    require_once '../functions/validation.php';
+?>
+
+<?php require_once '../includes/footer.php'; ?>
